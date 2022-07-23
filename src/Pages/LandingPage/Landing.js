ec5168cc45d28b6c5431db28d88bbc24f1c10fe1
Felipe Leão da Silva Dias <81325303+FelipeLeaoDias@users.noreply.github.com>
2022-07-23 00:13:44 -0300

    Revert "Develop"

--- src/Pages/LandingPage/Landing.js
+++ src/Pages/LandingPage/Landing.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import './landing.scss';
-import LandBox1 from '../../Components/LandBox1/LandBox';
-import LandBox2 from '../../Components/LandBox2/LandBox';
-import LandBox3 from '../../Components/LandBox3/LandBox';
 
 function Landing() {
     return (
-        <div className="containerA">
-            <LandBox1 />
-            <LandBox2 />
-            <LandBox3 />
+        <div>
+            <h1>Home Page</h1>
         </div>
-
-
     )
-}
+}  
 
 export default Landing
--- src/Pages/Projetos/Projetos.js
+++ src/Pages/Projetos/Projetos.js
@@ -1,32 +1,10 @@
 import React from 'react';
-import './projeto.scss';
-import ProjBox from '../../Components/ProjBox/ProjBox';
-import ProjBox2 from '../../Components/ProjBox2/ProjBox';
-import ProjBox3 from '../../Components/ProjBox3/ProjBox';
 
 function Projetos() {
     return (
-        
-        <div className="container"> 
-            <div className="texto-intro text-center slide-top">
-                <h1 className='display-2 text-uppercase'>Projetos</h1>
-                <p className='display-7'>
-
-                    O clube de progamação do SENAI/CIMATEC além de realizar torneios e aulas, também desenvolve projetos com os integrantes.
-                    Indo de websites, até programas funcionais para resolver determinados problemas. A maior parte dos projetos de código são disponibilizada no 
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/ClubeProgramacaoSSA"> GitHub </a>, e podem ser acessados
-                    pelos links abaixo. Outras documentações ou vídeos podem ser acessadas pelo <a target="_blank"
-                    rel="noopener noreferrer" href=''> Google Drive </a> do clube. Aqui estarão registrados os principais projetos que a equipe do Clube já realizou.
-       
-                </p>
-            </div>
-            <ProjBox className='auxiliar'/>
-            <ProjBox2 className='auxiliar'/>
-            <ProjBox3 className='auxiliar' />
-
+        <div>
+            <h1>Projetos Page</h1>
         </div>
-        
-        
     )
 }
 
