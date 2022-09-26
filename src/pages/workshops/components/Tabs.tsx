234d476528d49037eec5288bb7aadace62b3184a
João Zumaêta <ezzoscode@gmail.com>
2022-09-25 22:23:21 -0300

    [FEAT][SELECTOR] Workshopselector done.:sparkles:

--- src/pages/workshops/components/Tabs.tsx
+++ src/pages/workshops/components/Tabs.tsx
@@ -1,12 +1,12 @@
 import { Tab } from "@headlessui/react";
-import { GithubLogo, FilePdf } from "phosphor-react";
-import { Content } from "../index";
+import { ContentProps } from "../index";
+import { Workshop } from "./Workshop";
 
 //TODO: Active tab indentification
 
 
 interface TabsProps {
-  data: Content[];
+  data: ContentProps[];
 }
 
 export function Tabs({ data }: TabsProps) {
@@ -16,8 +16,8 @@ export function Tabs({ data }: TabsProps) {
         {data.map((item, index) => {
           return (
             <Tab key={index} className="flex gap-2 items-center">
-              <img className="w-12" src={item.logoUrl} alt={item.alt} />
-              {item.workshop}
+              <img className="w-12" src={item.iconUrl} alt={item.iconDescription} />
+              {item.workshopName}
             </Tab>
           );
         })}
@@ -26,41 +26,7 @@ export function Tabs({ data }: TabsProps) {
         {data.map((item, index) => {
           return (
             <Tab.Panel key={index}>
-              <h2 className="font-bold text-2xl mt-10 text-center mb-5">
-                {item.title}
-              </h2>
-              <div className="text-2xl font-semibold flex flex-col items-center gap-2">
-                <img
-                  className="w-13"
-                  src={item.avatarUrl}
-                  alt="Avatar do responsavel"
-                />
-                <div className="flex flex-col items-center">
-                  <span>{item.avatarName}</span>
-                  <span className="text-sm">{item.social}</span>
-                </div>
-              </div>
-              <p className="font-medium text-justify mt-10 text-xl mb-5">
-                {item.content}
-              </p>
-              <div className="flex flex-col gap-5 items-center mb-10 font-bold text-xl">
-                <a
-                  className="px-6 py-2 flex gap-3 items-center border-solid border-2 rounded-lg 
-                  border-[#FB9304] hover:scale-125 hover:bg-[#FB9304] duration-300"
-                  href=""
-                >
-                  <FilePdf size={32} />
-                  PDF
-                </a>
-                <a
-                  className="px-6 py-2 flex gap-3 items-center border-solid border-2 rounded-lg 
-                  border-[#AD00FF] hover:scale-125 hover:bg-[#AD00FF] duration-300"
-                  href=""
-                >
-                  <GithubLogo size={32} />
-                  GITHUB
-                </a>
-              </div>
+              <Workshop  workShop={item}/>
             </Tab.Panel>
           );
         })}
