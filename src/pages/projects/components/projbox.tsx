6a16aaa85ddd4c28576861073fcbd828daae40d9
Adrian Widmer <74932333+Awi-24@users.noreply.github.com>
2022-10-28 09:33:52 -0300

    [FEAT][PROJETOS]
    Primeiro Commit adicionando os primeiros recursos da tela de projetos.

--- src/pages/projects/components/projbox.tsx
+++ src/pages/projects/components/projbox.tsx
@@ -1,3 +1,15 @@
 export function Projbox() {
-	return <h1>Projbox</h1>;
+	return (
+		<main className="max-w-5xl">
+			<div className="text-white flex xsm:flex-col sm:flex-col flex-row sm:items-center xsm:items-center mt-10">
+				<div className="bg-orange rounded-xl pt-40 pb-40 pl-16 pr-16">
+					IMAGEM FICA AQUI
+				</div>
+				<div className="flex flex-col xsm:items-center sm:items-center items-start">
+					<h1 className="font-bold text-4xl mt-5 font-Roboto">TITULO</h1>
+					<p className="font-normal text-xl">Texto texto texto texto</p>
+				</div>
+			</div>
+		</main>
+	);
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,14 +1,15 @@
 //todo: fazer o componente do Projbox
 //todo: criar o caminho para puxar os projetos do banco de dados
+import {Projbox} from "../projects/components/projbox";	
 
 export function Projects() {
 	return (
 		<main>
 			<div className="text-white max-w-4xl mx-auto my-20 flex flex-col items-center">
-				<h1 className="my-8 font-Roboto font-bold md:text-6xl text-5xl ">
+				<h1 className="my-8 font-Roboto font-bold md:text-6xl text-6xl ">
 					<span className="text-orange">P</span>ROJETOS
 				</h1>
-				<p className="text-center font-Manrope mx-6 text-lg">
+				<p className="text-center font-Manrope mx-6 font-base text-xl">
 					O clube de progamação do SENAI/CIMATEC além de realizar torneios e
 					aulas, também desenvolve projetos com os integrantes. Indo de
 					websites, até programas funcionais para resolver determinados
@@ -37,6 +38,8 @@ export function Projects() {
 					do Clube já realizou.
 				</p>
 			</div>
+
+			<Projbox />
 		</main>
 	);
 }
