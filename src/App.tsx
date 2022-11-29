3e0ae6d734da3c3f0553eed291fdcf5f29f17148
Felipe Leão da Silva Dias <81325303+FelipeLeaoDias@users.noreply.github.com>
2022-11-29 15:36:47 -0300

    Implementando Navbar Basica
    Por falta de entendimento do Tailwind não foi possível implementar o Navbar proposto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Router";
+import NavBar from "./components/Navbar";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Router />
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
