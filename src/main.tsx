024a2356bbf9cd7818d4b94744f9e5888ce604c9
Pedro Mariotti <mariottipedro14@gmail.com>
2023-07-11 15:41:15 -0300

    Refactor init commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/home.tsx';
-import './index.css';
+import App from './App.tsx';
+import './styles/global.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: { 'orange': '#FF8A00' },
-    fontFamily: {
-      'pfont': 'Barlow Semi Condensed', //fonte dos paragrafos <p>
-    },
+    extend: {},
   },
+  plugins: [],
 };
