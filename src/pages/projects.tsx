f81893134d3221fa80c59c33cb234a09f2fa93e4
Pedro Mariotti <mariottipedro14@gmail.com>
2023-07-18 19:08:34 -0300

    Init pag projetos

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,28 +1,29 @@
-import { useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
+import NavBar from '../components/navbar';
+import Search from '../components/search';
 
 function Projects() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        {/* <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p> */}
+      <NavBar />
+      <div className="font-barlow pt-48 flex flex-col gap-52">
+        <header className="px-72 font-inter flex flex-col items-center gap-24">
+          <h1 className="text-9xl font-bold">PROJETOS</h1>
+          <p className="text-5xl text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            represhenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </header>
+        <main>
+          <Search />
+          <section id="projetos-principais"></section>
+          <div id="divider"></div>
+          <section id="outros-projetos"></section>
+        </main>
       </div>
       {/* <p className="">Click on the Vite and React logos to learn more</p> */}
     </>
